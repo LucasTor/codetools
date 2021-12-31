46877b51934e0bd6d8b23cbb23638345d8a717b4
Lucas Soranzo <lucasdeonsoranzo@gmail.com>
2021-12-30 21:59:07 -0300

    Add makefile and open source tools menu

--- codetools/knowledge/urls.py
+++ codetools/knowledge/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from codetools.knowledge.views import apis
+from codetools.knowledge.views import apis, open_source_tools
 
 app_name = "knowledge"
 
 urlpatterns = [
-    path('', apis, name="apis"),
+    path('apis', apis, name="apis"),
+    path('open-source-tools', open_source_tools, name="open-source-tools"),
 ]
